56681df2fc2b7c807934881021e8004a93f8c74f
boutina <boutina@AzariasB>
2015-10-29 15:46:06 +0000

    changing to POO

--- background.js
+++ background.js
@@ -14,102 +14,10 @@
 
 
 /* global chrome, competitions_participated */
-var flagsLang = {
-    "English": 1,
-    "German": 2,
-    "French": 3,
-    "Portuguese": 4,
-    "Spanish": 5,
-    "Indonesian": 6,
-    "Turkish": 7,
-    "Vietnamese": 8,
-    "Polish": 9,
-    "Romanian": 10,
-    "Malaysian": 11,
-    "Norwegian": 12,
-    "Persian": 13,
-    "Hungarian": 14,
-    "Chinese (Traditional)": 15,
-    "Chinese (Simplified)": 16,
-    "Danish": 17,
-    "Dutch": 18,
-    "Swedish": 19,
-    "Italian": 20,
-    "Finnish": 21,
-    "Serbian": 22,
-    "Catalan": 23,
-    "Filipino": 24,
-    "Croatian": 25,
-    "Russian": 26,
-    "Arabic": 27,
-    "Bulgarian": 28,
-    "Japanese": 29,
-    "Korean": 31,
-    "Greek": 32,
-    "Czech": 33,
-    "Estonian": 34,
-    "Latvian": 35,
-    "Hebrew": 36,
-    "Urdu": 37,
-    "Galician": 38,
-    "Lithuanian": 39,
-    "Georgian": 40,
-    "Armenian": 41,
-    "Kurdish": 42,
-    "Azerbaijani": 43,
-    "Hindi": 44,
-    "Slovak": 45,
-    "Slovenian": 46,
-    "Icelandic": 48,
-    "Thai": 50,
-    "Pashto": 51,
-    "Esperanto": 52
-};
+var flagsLang = {"English": 1, "German": 2, "French": 3, "Portuguese": 4, "Spanish": 5, "Indonesian": 6, "Turkish": 7, "Vietnamese": 8, "Polish": 9, "Romanian": 10, "Malaysian": 11, "Norwegian": 12, "Persian": 13, "Hungarian": 14, "Chinese (Traditional)": 15, "Chinese (Simplified)": 16, "Danish": 17, "Dutch": 18, "Swedish": 19, "Italian": 20, "Finnish": 21, "Serbian": 22, "Catalan": 23, "Filipino": 24, "Croatian": 25, "Russian": 26, "Arabic": 27, "Bulgarian": 28, "Japanese": 29, "Korean": 31, "Greek": 32, "Czech": 33, "Estonian": 34, "Latvian": 35, "Hebrew": 36, "Urdu": 37, "Galician": 38, "Lithuanian": 39, "Georgian": 40, "Armenian": 41, "Kurdish": 42, "Azerbaijani": 43, "Hindi": 44, "Slovak": 45, "Slovenian": 46, "Icelandic": 48, "Thai": 50, "Pashto": 51, "Esperanto": 52};
 
+var connector = new Connector();
 
-var connected = false;
-var websiteUrl = 'http://10fastfingers.com/';
-var lastCompetition;
-var nwCompetitions = 0;
-var languageTestVal = 'english';
-var languageTextName = 'English';
-var refreshTimeout = 10;
-var valRegex = /\s*var\s+competitions_participated\s*=\s*\[(\"\d+\",)*(\"\d+\")?\];/;
-var scripReg = new RegExp('<script[\\s\\S\\d\\D]*?>[\\s\\S]*?</script>', 'g');
-var linkReg = new RegExp('<link[\\d\\D]*?>', 'g');
-var competParticipated;
-var currentTimeout;
-
-
-function is10fastFingersUrl(url) {
-    return url.indexOf(websiteUrl) === 0;
-}
-
-function getCompetitionURl(competRef) {
-    return websiteUrl + competRef;
-}
-
-function getTypingTestUrl() {
-    return websiteUrl + 'typing-test/' + languageTestVal;
-}
-
-function getFlagId() {
-    return 'flagid' + flagsLang[languageTextName];
-}
-
-//Remove all scripts tag of the text
-function removeScript(text) {
-    return text.replace(scripReg, "");
-}
-
-//Remove all links tag of the text
-function removeLink(text) {
-    return text.replace(linkReg, "");
-}
-
-function cleanHTML(html) {
-    return removeLink(removeScript(html));
-}
 
 /*
  * Set all the basics options
@@ -134,15 +42,17 @@ function setOptions(options) {
  */
 function init() {
     listenToStorage();
-    chrome.alarms.onAlarm.addListener(function (alarm) {
-        refresh();
+    chrome.alarms.onAlarm.addListener(function (alarm) {//Add alarm listener
+        connector.refresh();
     });
-    chrome.browserAction.onClicked.addListener(function () {
+    chrome.browserAction.onClicked.addListener(function () {//Add click listnere
         click();
     });
+
+    //Take out options and let's go !
     chrome.storage.sync.get(['favLangName', 'favLangVal', 'refreshTimeout'], function (items) {
         setOptions(items);
-        refresh();
+        connector.refresh();
     });
 }
 
@@ -151,11 +61,11 @@ function init() {
  * handler
  */
 function click() {
-    refresh(function () {
-        if (connected) {
-            if (nwCompetitions > 0) {
-                openCompetition(lastCompetition);
-                refreshCompetitions(1.2); //Refresh competition after 2 minutes of going in
+    connector.refresh(function () {
+        if (connector.connected) {
+            if (connector.nwCompetitions > 0) {
+                openCompetition(connector.lastCompetition);
+                connector.refreshCompetitions(1.2); //Refresh competition after 2 minutes of going in
             } else {
                 openFastFingers();
             }
@@ -163,130 +73,188 @@ function click() {
     });
 }
 
-/*
- * Check if the user is connected to the website,
- * if he is, check for new competitions
- * 
- * @param {function} callback
- */
-function refresh(callback) {
-    chrome.cookies.get({
-        'url': 'http://10fastfingers.com/',
-        'name': 'CakeCookie[rememberMe]'
-    }, function (cookie) {
-        checkIfConnected(cookie);
-        callback && callback();
-    });
-}
 
-/*
- * Handler when the cookie is found
- * 
- * @param {Cookie} cookie
- */
-function checkIfConnected(cookie) {
-    if (cookie === null) {//not connected
-        connected = false;
-        chrome.browserAction.setIcon({path: "icon _gray.png"});
-        chrome.browserAction.setTitle({title: "Not connected to 10fastfingers"});
-        chrome.browserAction.setBadgeBackgroundColor({color: [190, 190, 190, 230]});
-        chrome.browserAction.setBadgeText({text: "?"});
-        refreshCompetitions(refreshTimeout);
-    } else {
-        connected = true;
-        lookForNewCompetitions();
-    }
-}
+function Connector() {
+    this.connected = false;
+    this.websiteUrl = 'http://10fastfingers.com/';
+    this.lastCompetition;
+    this.nwCompetitions = 0;
+    this.languageTestVal = 'english';
+    this.languageTextName = 'English';
+    this.refreshTimeout = 10;
+    this.valRegex = /\s*var\s+competitions_participated\s*=\s*\[(\"\d+\",)*(\"\d+\")?\];/;
+    this.scripReg = new RegExp('<script[\\s\\S\\d\\D]*?>[\\s\\S]*?</script>', 'g');
+    this.linkReg = new RegExp('<link[\\d\\D]*?>', 'g');
+    this.competParticipated;
+    this.currentTimeout;
+
+    var self = this;
 
 
-/*
- * Get the 'competitions' page and process
- * it to see if there are new competitions
- * passing by : taking the values of the 'alreadyDone' competitions
- */
-function lookForNewCompetitions() {
-    if (HTMLinXHR()) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", websiteUrl + "competitions", true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                // innerText does not let the attacker inject HTML elements.
-                var res = valRegex.exec(xhr.responseText);
-                eval(res[0]);//Get "competition_participated" var.
-                competParticipated = competitions_participated;
-                var clearedHTML = cleanHTML(xhr.responseText);
-                var dummyDiv = document.createElement('DIV');
-                dummyDiv.innerHTML = clearedHTML;
-                document.body.appendChild(dummyDiv);
-                var core = document.getElementById('join-competition-table');
-                processCore(core.getElementsByTagName('tbody')[0]);
+    //---------------------------------------//
+    //          PUBLIC FUNCTIONS             //
+    //---------------------------------------//
+    /*
+     * Check if the user is connected to the website,
+     * if he is, check for new competitions
+     * 
+     * @param {function} callback
+     */
+
+    this.refresh = function (callback) {
+        chrome.cookies.get({
+            'url': this.websiteUrl,
+            'name': 'CakeCookie[rememberMe]'
+        }, function (cookie) {
+            checkIfConnected(cookie, callback);
+        });
+    };
+
+    /*
+     * Set the alarm
+     * if the timeout is the same as before, nothing changes
+     * if the timeout is different, changin the periodInterval
+     */
+    this.refreshCompetitions = function (timeout/*in minutes*/) {
+        chrome.alarms.get('refresh', function (alarm) {
+            if (alarm) {
+                if (alarm.periodInMinutes !== timeout) {
+                    chrome.alarms.clear('refresh', function () {
+                        chrome.alamrs.create('refresh', {periodInMinutes: timeout});
+                    });
+                }
+            } else {
+                chrome.alarms.create('refresh', {periodInMinutes: timeout});
             }
-        };
-        xhr.send();
-    } else {
-        console.log("Not possible");
-    }
-}
+        });
+    };
 
-/*
- * Check for each competition if already done
- * and if not, add a new "unDone" competition.
- */
-function processCore(tbody) {
-    nwCompetitions = 0;
-    var competitions = tbody.getElementsByTagName('tr');
-    for (var i = 0; i < competitions.length; i++) {
-        checkCompetition(competitions[i]);
-    }
-    if (nwCompetitions > 0 && lastCompetition) {
-        chrome.browserAction.setTitle({title: nwCompetitions === 1 ? 'There is a new competition' :
-                    'There are new competitions'});
-        chrome.browserAction.setBadgeBackgroundColor({color: [58, 214, 0, 255]});
-    } else {
-        chrome.browserAction.setTitle({title: 'Not new competitions'});
-        chrome.browserAction.setBadgeBackgroundColor({color: [190, 190, 190, 230]});
-    }
-    chrome.browserAction.setBadgeText({text: ("" + nwCompetitions) || ("" + 0)});
-    refreshCompetitions(refreshTimeout);//Refresh to see if there is a new competition every 10 minutes
-}
+    this.is10fastFingersUrl = function (url) {
+        return url.indexOf(this.websiteUrl) === 0;
+    };
 
-/*
- * Set the alarm
- * if the timeout is the same as before, nothing changes
- * if the timeout is different, changin the periodInterval
- */
-function refreshCompetitions(timeout/*in minutes*/) {
-    chrome.alarms.get('refresh', function (alarm) {
-        if (alarm) {
-            if (alarm.periodInMinutes !== timeout) {
-                chrome.alarms.clear('refresh', function () {
-                    chrome.alamrs.create('refresh', {periodInMinutes: timeout});
-                });
-            }
+    this.getCompetitionURl = function (competRef) {
+        return this.websiteUrl + competRef;
+    };
+
+    this.getTypingTestUrl = function () {
+        return this.websiteUrl + 'typing-test/' + languageTestVal;
+    };
+
+
+    //---------------------------------------//
+    //          PRIVATE FUNCTIONS            //
+    //---------------------------------------//
+
+    function getFlagId() {
+        return 'flagid' + flagsLang[languageTextName];
+    }
+
+    //Remove all scripts tag of the text
+    function removeScript(text) {
+        return text.replace(self.scripReg, "");
+    }
+
+    //Remove all links tag of the text
+    function removeLink(text) {
+        return text.replace(self.linkReg, "");
+    }
+
+    function cleanHTML(html) {
+        return removeLink(removeScript(html));
+    }
+
+    /*
+     * Handler when the cookie is found
+     * 
+     * @param {Cookie} cookie
+     */
+    function checkIfConnected(cookie, callback) {
+        if (cookie === null) {//not connected
+            connected = false;
+            chrome.browserAction.setIcon({path: "icon _gray.png"});
+            chrome.browserAction.setTitle({title: "Not connected to 10fastfingers"});
+            chrome.browserAction.setBadgeBackgroundColor({color: [190, 190, 190, 230]});
+            chrome.browserAction.setBadgeText({text: "?"});
+            refreshCompetitions(refreshTimeout);
         } else {
-            chrome.alarms.create('refresh', {periodInMinutes: timeout});
-        }
-    });
-}
-
-/*
- * For one given competition, check if the 
- * competition is in the 'done' list, and if not, add new undone competition.
- * 
- * @param {DOMObject} compet
- */
-function checkCompetition(compet) {
-    var informations = compet.getElementsByTagName('td');
-    var flag = informations[0].getElementsByTagName('span')[0].getAttribute('id');
-    //France flag => TO CHANGE
-    if (flag === getFlagId()) {
-        var alreadyDone = informations[2].getElementsByTagName('div')[0].getAttribute('competition_id');
-        if (competParticipated.indexOf(alreadyDone) === -1) {//Compet not done yet
-            var ref = informations[1].getElementsByTagName('a')[0].getAttribute('href');
-            nwCompetitions++;
-            lastCompetition = ref;
+            connected = true;
+            lookForNewCompetitions(callback);
         }
     }
+
+    /*
+     * Get the 'competitions' page and process
+     * it to see if there are new competitions
+     * passing by : taking the values of the 'alreadyDone' competitions
+     */
+    function lookForNewCompetitions(callback) {
+        if (HTMLinXHR()) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", self.websiteUrl + "competitions", true);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    // innerText does not let the attacker inject HTML elements.
+                    var res = self.valRegex.exec(xhr.responseText);
+                    eval(res[0]);//Get "competition_participated" var.
+                    self.competParticipated = competitions_participated;
+                    var clearedHTML = cleanHTML(xhr.responseText);
+                    var dummyDiv = document.createElement('DIV');
+                    dummyDiv.innerHTML = clearedHTML;
+                    document.body.appendChild(dummyDiv);
+                    var core = document.getElementById('join-competition-table');
+                    processCore(core.getElementsByTagName('tbody')[0]);
+                    callback && callback();
+                }
+            };
+            xhr.send();
+        } else {
+            console.log("Not possible");
+        }
+    }
+
+    /*
+     * Check for each competition if already done
+     * and if not, add a new "unDone" competition.
+     */
+    function processCore(tbody) {
+        self.nwCompetitions = 0;
+        var competitions = tbody.getElementsByTagName('tr');
+        for (var i = 0; i < competitions.length; i++) {
+            checkCompetition(competitions[i]);
+        }
+        if (this.nwCompetitions > 0 && lastCompetition) {
+            chrome.browserAction.setTitle({title: self.nwCompetitions === 1 ? 'There is a new competition' :
+                        'There are new competitions'});
+            chrome.browserAction.setBadgeBackgroundColor({color: [58, 214, 0, 255]});
+        } else {
+            chrome.browserAction.setTitle({title: 'Not new competitions'});
+            chrome.browserAction.setBadgeBackgroundColor({color: [0, 0, 0, 0]});
+        }
+        chrome.browserAction.setBadgeText({text: (self.nwCompetitions === 0 ? "" : (self.nwCompetitions + ""))});
+        self.refreshCompetitions(refreshTimeout);//Refresh to see if there is a new competition every 10 minutes
+    }
+
+    /*
+     * For one given competition, check if the 
+     * competition is in the 'done' list, and if not, add new undone competition.
+     * 
+     * @param {DOMObject} compet
+     */
+    function checkCompetition(compet) {
+        var informations = compet.getElementsByTagName('td');
+        var flag = informations[0].getElementsByTagName('span')[0].getAttribute('id');
+        if (flag === getFlagId()) {
+            var alreadyDone = informations[2].getElementsByTagName('div')[0].getAttribute('competition_id');
+            if (self.competParticipated.indexOf(alreadyDone) === -1) {//Compet not done yet
+                var ref = informations[1].getElementsByTagName('a')[0].getAttribute('href');
+                nwCompetitions++;
+                self.lastCompetition = ref.substr(1);
+            }
+        }
+    }
+
+
 }
 
 //check if we really have a html-like response
@@ -294,7 +262,7 @@ function HTMLinXHR() {
     if (!window.XMLHttpRequest)
         return false;
     var req = new window.XMLHttpRequest();
-    req.open('GET', websiteUrl + 'competitions', false);
+    req.open('GET', connector.websiteUrl + 'competitions', false);
     try {
         req.responseType = 'document';
     } catch (e) {
@@ -307,33 +275,29 @@ function HTMLinXHR() {
 function openFastFingers() {
     chrome.tabs.getAllInWindow(undefined, function (tabs) {
         for (var i = 0, tab; tab = tabs[i]; i++) {
-            if (tab.url && is10fastFingersUrl(tab.url)) {
+            if (tab.url && connector.is10fastFingersUrl(tab.url)) {
                 chrome.tabs.update(tab.id, {active: true});
                 return;
             }
         }
-        chrome.tabs.create({url: getTypingTestUrl()});
+        chrome.tabs.create({url: connector.getTypingTestUrl()});
     });
 }
 
 //Open the competition to the given id
 //The id must contain the 'competition/' url
 function openCompetition(competitionId) {
-    chrome.tabs.create({url: getCompetitionURl(competitionId)});
+    chrome.tabs.create({url: connector.getCompetitionURl(competitionId)});
 }
 
 //Check if the chrome storage is changin to quickly change the icon display.
 function listenToStorage() {
     chrome.storage.onChanged.addListener(function (items) {
-        languageTestVal = (items.favLangVal ? items.favLangVal.newValue : languageTestVal);
-        languageTextName = (items.favLangName ? items.favLangName.newValue : languageTextName);
-        refreshTimeout = (items.refreshTimeout ? items.refreshTimeout.newValue : refreshTimeout);
-        refresh();
+        connector.languageTestVal = (items.favLangVal ? items.favLangVal.newValue : languageTestVal);
+        connector.languageTextName = (items.favLangName ? items.favLangName.newValue : languageTextName);
+        connector.refreshTimeout = (items.refreshTimeout ? items.refreshTimeout.newValue : refreshTimeout);
+        connector.refresh();
     });
 }
 
 init();
-
-/*
- * <!DOCTYPE html>
- */
