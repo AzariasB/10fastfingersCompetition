ac5e66c9d8ac53a6f1c6dd6a22023da86a10b4d8
azarias.boutin <azarias.boutin@luxquanta.com>
2024-01-11 18:09:30 +0100

    Fixed build error

--- src/options/OptionForm.ts
+++ src/options/OptionForm.ts
@@ -24,7 +24,7 @@
 
 import "@materializecss/materialize/dist/css/materialize.css";
 
-import { M } from "@materializecss/materialize";
+import { M } from "@materializecss/materialize/dist/js/materialize";
 import {
   Config,
   tr,
