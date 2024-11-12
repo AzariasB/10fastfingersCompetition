a0d138e980200324c61e688010e02b92bf2ff094
azarias.boutin <azarias.boutin@luxquanta.com>
2024-11-12 19:16:37 +0100

    Add back makeManifestPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,33 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { resolve } from 'node:path'
+import { existsSync, mkdirSync, writeFileSync } from 'node:fs'
+import manifest from './src/manifest'
 
 const root = resolve(__dirname, 'src')
 const outDir = resolve(__dirname, 'dist')
 
+const makeManifestPlugin = {
+  name: 'make-manifest',
+  buildEnd(err) {
+    if (err) {
+      return
+    }
+    if (!existsSync(outDir)) {
+      mkdirSync(outDir)
+    }
+
+    const manifestPath = resolve(outDir, 'manifest.json')
+
+    writeFileSync(manifestPath, JSON.stringify(manifest, null, 2))
+
+    console.log(`\nManifest file copy complete: ${manifestPath}`, 'success')
+  },
+}
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), makeManifestPlugin],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
